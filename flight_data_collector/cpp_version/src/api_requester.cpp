f77b69a06c4c00e25e4c8e822a4e6358f14d4fd3
Siraj Hassan <sirajhassan@yahoo.com>
2020-07-10 17:44:44 -0400

    now writes a json file out

--- flight_data_collector/cpp_version/src/api_requester.cpp
+++ flight_data_collector/cpp_version/src/api_requester.cpp
@@ -5,57 +5,56 @@
 // g++ -std=c++11 api_requester.cpp -stdlib=libc++ -lcpprest -lssl -lcrypto -lboost_system -lboost_thread-mt -lboost_chrono-mt -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include
 
 
+#include <iostream>
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
-
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace web::http;                  // Common HTTP functionality
-using namespace web::http::client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
-
-int main(int argc, char* argv[])
-{
-    auto fileStream = std::make_shared<ostream>();
-
-    // Open stream to output file.
-    pplx::task<void> requestTask = fstream::open_ostream(U("results.html")).then([=](ostream outFile)
-    {
-        *fileStream = outFile;
-
-        // Create http_client to send the request.
-        http_client client(U("http://www.bing.com/"));
-
-        // Build request URI and start the request.
-        uri_builder builder(U("/search"));
-        builder.append_query(U("q"), U("cpprestsdk github"));
-        return client.request(methods::GET, builder.to_string());
-    })
-
-    // Handle response headers arriving.
-    .then([=](http_response response)
-    {
-        printf("Received response status code:%u\n", response.status_code());
-
-        // Write response body into the file.
-        return response.body().read_to_end(fileStream->streambuf());
-    })
-
-    // Close the file stream.
-    .then([=](size_t)
-    {
-        return fileStream->close();
-    });
-
-    // Wait for all the outstanding I/O to complete and handle any exceptions
-    try
-    {
-        requestTask.wait();
-    }
-    catch (const std::exception &e)
-    {
-        printf("Error exception:%s\n", e.what());
-    }
-
-    return 0;
+#include <cpprest/uri.h>
+#include <cpprest/json.h>
+ 
+using namespace utility;
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace concurrency::streams;
+ 
+int main() {
+	// Create a file stream to write the received file into it.
+	auto fileStream = std::make_shared<ostream>();
+	
+	// Open stream to output file.
+	pplx::task<void> requestTask = fstream::open_ostream(U("users.json"))
+	
+	// Make a GET request.
+	.then([=](ostream outFile) {
+		*fileStream = outFile;
+ 
+		// Create http_client to send the request.
+		http_client client(U("https://opensky-network.org"));
+ 
+		// Build request URI and start the request.
+		return client.request(methods::GET, uri_builder(U("api")).append_path(U("states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226")).to_string());
+	})
+ 
+	// Get the response.
+	.then([=](http_response response) {
+		// Check the status code.
+		if (response.status_code() != 200) {
+			throw std::runtime_error("Returned " + std::to_string(response.status_code()));
+		}
+ 
+		// Write the response body to file stream.
+		response.body().read_to_end(fileStream->streambuf()).wait();
+ 
+		// Close the file.
+		return fileStream->close();
+	});
+ 
+	// Wait for the concurrent tasks to finish.
+	try {
+		while (!requestTask.is_done()) { std::cout << ""; }
+	} catch (const std::exception &e) {
+		printf("Error exception:%s\n", e.what());
+	}
+ 
+	return 0;
 }
