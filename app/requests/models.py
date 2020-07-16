67c7f1dc29f57d3681644eccfe99f57982744d1e
Siraj Hassan <sirajhassan@yahoo.com>
2020-07-16 18:05:08 -0500

    api url works

--- app/requests/models.py
+++ app/requests/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from rest_framework.reverse import reverse as api_reverse
+
+
+
+class Request(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	lat = models.DecimalField( max_digits = 10, decimal_places = 5, null = True, blank = True) 
+	lng = models.DecimalField( max_digits = 10, decimal_places = 5, null = True, blank = True )
+	timestamp = models.DateTimeField(auto_now_add = True)
+
+	def __str__(self):
+		return str(self.user)
+
+
