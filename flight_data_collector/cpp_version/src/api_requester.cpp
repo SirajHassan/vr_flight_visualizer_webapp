8240ca8501e568e2ff2d6008b1ff49a1335db614
Siraj Hassan <sirajhassan@yahoo.com>
2020-07-28 16:55:38 -0500

    removed commented out code

--- flight_data_collector/cpp_version/src/api_requester.cpp
+++ flight_data_collector/cpp_version/src/api_requester.cpp
@@ -33,72 +33,6 @@ using namespace concurrency::streams;
 
 int send_request(std::string lamin, std::string lomin, std::string lamax, std::string lomax);
 
-// int main() {
-
-// 	// fake args
-// 	int r1[5] = {1,45,5,47,10};
-	
-// 	//build api request.. 
-// 	std::ostringstream os;
-// 	std::string lamin = std::to_string(r1[1]);
-// 	std::string lomin = std::to_string(r1[2]);
-// 	std::string lamax = std::to_string(r1[3]);
-// 	std::string lomax = std::to_string(r1[4]);
-// 	os << "states/all?lamin=" << lamin << "&lomin="<< lomin <<"&lamax="<< lamax <<"&lomax="<< lomax << ""; //"states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226"
-// 	std::string requestPath = os.str();
-
-
-// 	// Create a file stream to write the received file into it.
-// 	auto fileStream = std::make_shared<ostream>();
-	
-// 	// Open stream to output file.
-// 	pplx::task<void> requestTask = fstream::open_ostream(U("flights.json"))
-	
-// 	// Make a GET request.
-// 	.then([=](ostream outFile) {
-// 		*fileStream = outFile;
- 
-// 		// Create http_client to send the request.
-// 		http_client client(U("https://opensky-network.org"));
- 
-// 		// Build request URI and start the request.
-// 		return client.request(methods::GET, uri_builder(U("api")).append_path(U(requestPath)).to_string());
-// 	})
- 
-// 	// Get the response.
-// 	.then([=](http_response response) {
-// 		// Check the status code.
-// 		if (response.status_code() != 200) {
-// 			throw std::runtime_error("Returned " + std::to_string(response.status_code()));
-// 		}
- 
-// 		// Write the response body to file stream.
-// 		// response.body().read_to_end(fileStream->streambuf()).wait(); // reads to file..you can only do this or extract json, not both
-		
-// 		// Close the file.
-// 		//return fileStream->close();
-// 		// Read as Json
-// 		return response.extract_json();
-// 	})
-// 	.then([=](json::value jsonObject) {
-// 		std::cout << jsonObject <<std::endl ;
-// 		return fileStream->close();
-// 	})
-
-// 	;
-
- 
-	
-// 	// Wait for the concurrent tasks to finish.
-// 	try {
-// 		while (!requestTask.is_done()) { std::cout << ""; }
-// 	} catch (const std::exception &e) {
-// 		printf("Error exception:%s\n", e.what());
-// 	}
- 
-// 	return 0;
-// }
-
 int main() {
 
 	// fake args
@@ -138,10 +72,7 @@ int main() {
  
 	// Parse the user details.
 	.then([](json::value jsonObject) {
-		std::cout << jsonObject[0];
-		// std::cout << jsonObject[U("time")].as_integer()
-		// 	<< " " << jsonObject[U("states")].as_string()
-		// 	<< std::endl;
+		std::cout << jsonObject[0]; 
 	});
  
 	// Wait for the concurrent tasks to finish.
